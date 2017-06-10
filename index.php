7ba0a8c4634bcb96d9d982a6bb47fffcbeffac61
Victor <Victor@Vicpc>
2017-06-10 11:18:48 +0200

    Proyecto original 69

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         echo "hola mundo","<br>";
+        echo "hola mundo","<br>";
  
         define("constante","valor1");
         $a=78;
